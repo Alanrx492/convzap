8780ea96defd7e1162f69d35629fa3de07fc7c7e
Alanrx492 <alan-carvalho2014@outlook.com>
2025-06-04 23:14:15 -0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,24 @@
+// Facebook Pixel
+(function(f: any, b: any, e: any, v: any, n?: any, t?: any, s?: any) {
+  if (f.fbq) return;
+  n = f.fbq = function () {
+    n.callMethod ? n.callMethod.apply(n, arguments) : n.queue.push(arguments);
+  };
+  if (!f._fbq) f._fbq = n;
+  n.push = n;
+  n.loaded = true;
+  n.version = '2.0';
+  n.queue = [];
+  t = b.createElement(e);
+  t.async = true;
+  t.src = v;
+  s = b.getElementsByTagName(e)[0];
+  s.parentNode.insertBefore(t, s);
+})(window, document, 'script', 'https://connect.facebook.net/en_US/fbevents.js');
+
+window.fbq('init', '730585529657541');
+window.fbq('track', 'PageView');
+
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
